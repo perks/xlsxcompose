d588c60da1cccbb62b02371f98ef579a5af93d10
Chris Evans <chris.evans93@gmail.com>
2014-11-16 04:19:43 -0500

    Loop working, optimized speed

--- xlsxcompose.py
+++ xlsxcompose.py
@@ -14,17 +14,17 @@ def compose(input, output, start_row, end_row, mappings, ss, ts):
 
     rb = open_workbook(input)
     r_sheet = rb.sheet_by_name(ss)
+    ref = [x[1] for x in mappings]
+    filtered_cols = filter(lambda tup: tup[1] in ref, [(i, x) for i, x in enumerate(r_sheet.row_values(0))])
 
-    workbook = xlsxwriter.Workbook(output)
-    worksheet = workbook.add_worksheet(ts)
 
     columns = {}
 
-    for col_index, mapping in enumerate(mappings):
+    for mapping in mappings:
         target_header = mapping[0]
         orig_header = mapping[1]
 
-        for i in range(1, r_sheet.ncols):
+        for (i, headers) in filtered_cols:
             col_values = filter(None, r_sheet.col(i))
             col_values = map(lambda x: str(int(x.value)) if x.ctype == 2 else x.value, col_values)
             col_head = col_values[0]
@@ -33,14 +33,27 @@ def compose(input, output, start_row, end_row, mappings, ss, ts):
                 columns.update({target_header: col_values})
                 break
 
-        if columns.has_key(target_header):
-            write_col = [target_header] + (columns[target_header][START_ROW:END_ROW] if END_ROW else columns[target_header][START_ROW:])
-            for row_index, cell in enumerate(write_col):
-                cell_write(worksheet, row_index, col_index, cell)
-        else:
-            cell_write(worksheet, 0, col_index, target_header)
 
-    workbook.close()
+    count = 0
+    while (count < 3):
+        print "top loop"
+        file_name = "{}_{}".format(count , output)
+        workbook = xlsxwriter.Workbook(file_name)
+        worksheet = workbook.add_worksheet(ts)
+
+        for col_index, mapping in enumerate(mappings):
+            target_header = mapping[0]
+            orig_header = mapping[1]
+
+            if columns.has_key(target_header):
+                write_col = [target_header] + (columns[target_header][START_ROW:END_ROW] if END_ROW else columns[target_header][START_ROW:])
+                for row_index, cell in enumerate(write_col):
+                    cell_write(worksheet, row_index, col_index, cell)
+            else:
+                cell_write(worksheet, 0, col_index, target_header)
+
+        workbook.close()
+        count += 1
 
 def cell_write(sheet,row_index, col_index, value):
         sheet.write(row_index, col_index, value)
